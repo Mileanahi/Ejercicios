a28e636fee90738bb623dbbc04a1cd48f62e75d8
Milena Moyano <mileanahimm039@gmail.com>
2026-09-18 21:32:58 -0500

    Creacion de la clase base

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/* 
+Titulo: Examen de Java POO
+Autora: Milena Moyano
+fecha: 18 de septiembre del 2026 
+Una academia de videojuegos necesita un programa para registrar personajes. Todos comparten un
+identificador, un nombre y un nivel, pero cada tipo realiza una acción diferente. El sistema debe guardar los
+objetos en una sola colección, mostrarlos y buscar uno por su identificador.
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
